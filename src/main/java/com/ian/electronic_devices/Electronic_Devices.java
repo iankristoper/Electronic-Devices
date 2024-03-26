deeb28275cbdb1c2fa0641f7cc774db5475d063e
iankristoper <iankristoper26@gmail.com>
2024-03-26 11:34:56 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/electronic_devices/Electronic_Devices.java
@@ -0,0 +1,18 @@
+package com.ian.electronic_devices;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Electronic_Devices 
+{
+
+    public static void main(String[] args) 
+    {
+        Laptop acer = new Laptop("acer", "aspire 1111", 1080, "linux");
+        SmartPhone iphone = new SmartPhone("iphone", "iphone12", 1000, "mac");
+        
+        
+    }
+}
