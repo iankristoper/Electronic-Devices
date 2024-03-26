521e388590be85321ff52892e6341ef86050af28
iankristoper <iankristoper26@gmail.com>
2024-03-26 11:35:22 +0800

    UPDATE: sub class features

---
+++ src/main/java/com/ian/electronic_devices/Laptop.java
@@ -0,0 +1,18 @@
+package com.ian.electronic_devices;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Laptop extends Device
+{
+    //passing data to constructor
+    public Laptop(String brand, String model, double screenSize, String operatingSystem)
+    {
+        super(brand, model, screenSize, operatingSystem);
+    }
+    
+    
+    
+}
